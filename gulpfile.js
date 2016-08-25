c7e381decb4a58e6253dad924d20accd1c5a507d
vlmuzychenko <vlad.muzychenko@gmail.com>
2016-08-25 19:34:45 +0300

    gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp         = require('gulp'),
+	  sass         = require('gulp-sass'),
+	  browserSync  = require('browser-sync'),
+	  concat       = require('gulp-concat'),
+  	uglify       = require('gulp-uglifyjs'),
+	  del          = require('del');
+	  cssmin       = require('gulp-cssmin'),
+	  rename       = require('gulp-rename'),
+	  autoprefixer = require('gulp-autoprefixer'),
+ 	  imagemin     = require('gulp-imagemin');
+
+gulp.task('sass', function() {
+	return gulp.src('src/sass/**/*.sass')
+		.pipe(sass())
+		.pipe(autoprefixer(['last 15 versions', '> 1%']))
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('cssmin', ['sass'], function() {
+	return gulp.src('src/css/main.css')
+	.pipe(cssmin())
+	.pipe(rename({suffix: '.min'}))
+	.pipe(gulp.dest('src/css'));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'src'
+		},
+		notify: false
+	});
+});
+
+gulp.task('imagemin', function() {
+	gulp.src('src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
+gulp.task('watch', ['browser-sync', 'cssmin', 'scripts', 'csslibs'], function() {
+	gulp.watch('src/sass/**/*.sass', ['sass']);
+	gulp.watch('src/css/**/*.css', ['cssmin']);
+	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('build', ['clean', 'sass', 'scripts', 'imagemin'], function() {
+	var buildCss = gulp.src([
+		'src/css/main.min.css',
+	])
+	.pipe(gulp.dest('dist/css'));
+
+	var buildFonts = gulp.src('src/fonts/**/*')
+	.pipe(gulp.dest('dist/fonts'));
+
+	var buildJs = gulp.src('src/js/**/*')
+	.pipe(gulp.dest('dist/js'));
+
+	var buildHtml = gulp.src('src/*.html')
+	.pipe(gulp.dest('dist'));
+});
